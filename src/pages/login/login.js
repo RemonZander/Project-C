2ca480dd41e87ce6e41dc29eceaa728cd7fe138c
guido <guido@DESKTOP-F3UV7F8>
2021-10-27 20:10:01 +0200

    Login UI issues gefixt + admin-portal erbij gemaakt

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,30 +1,146 @@
 import './login.css';
 import kynda from './kynda.png';
+import { doc } from 'prettier';
+import { useState } from 'react';
 
 export default {
     url: '/',
     render: (queryParams) => {
+        const [isAdminPortal, setIsAdminPortal] = useState(false);
+        const array = [
+            <p id="passwordShort">U wachtwoord is te kort</p>,
+            <p id="passwordNoLower">U wachtwoord bevat geen kleine letter</p>,
+            <p id="passwordNoUpper">U wachtwoord bevat geen hoofdletter</p>,
+            <p id="passwordNoNumber">U wachtwoord bevat geen cijfer</p>,
+            <p id="emailApenstaartje">Uw email bevat geen apenstaartje</p>,
+            <p id="emailSpatie">Uw email bevat een spatie</p>,
+        ];
         return (
             <div className="center">
+                <div id="message" className="message">
+                    {array}
+                </div>
                 <img src={kynda} alt="Kynda logo" className="image"></img>
-                <h2>Log hier in op het klant-portal</h2>
+                <h2>
+                    {isAdminPortal
+                        ? 'Log in op uw admin-portaal'
+                        : 'Log in op uw klanten-portaal'}
+                </h2>
                 <form method="post">
                     <div className="txt_field">
-                        <input type="text" required></input>
+                        <input
+                            type="text"
+                            onInput={(e) => {
+                                const message =
+                                    document.getElementById('message');
+                                const showMessage = (el, cond) =>
+                                    cond
+                                        ? (el.style.display = 'block')
+                                        : (el.style.display = 'none');
+                                if (e.target.value.length > 0) {
+                                    message.style.display = 'block';
+                                    showMessage(
+                                        document.getElementById(
+                                            'emailApenstaartje'
+                                        ),
+                                        e.target.value.indexOf('@') < 0
+                                    );
+                                    showMessage(
+                                        document.getElementById('emailSpatie'),
+                                        e.target.value.indexOf(' ') >= 0
+                                    );
+                                } else {
+                                    message.style.display = 'none';
+                                }
+                            }}
+                            id="email"
+                            required
+                        ></input>
                         <span></span>
                         <label>Email</label>
                     </div>
                     <div className="txt_field">
-                        <input type="password" required></input>
+                        <input
+                            type="password"
+                            onInput={(e) => {
+                                const message =
+                                    document.getElementById('message');
+                                const showMessage = (el, cond) =>
+                                    cond
+                                        ? (el.style.display = 'block')
+                                        : (el.style.display = 'none');
+                                if (e.target.value.length > 0) {
+                                    message.style.display = 'block';
+                                    showMessage(
+                                        document.getElementById(
+                                            'passwordShort'
+                                        ),
+                                        e.target.value.length < 8
+                                    );
+                                    showMessage(
+                                        document.getElementById(
+                                            'passwordNoLower'
+                                        ),
+                                        e.target.value.search(/[a-z]/) < 0
+                                    );
+                                    showMessage(
+                                        document.getElementById(
+                                            'passwordNoUpper'
+                                        ),
+                                        e.target.value.search(/[A-Z]/) < 0
+                                    );
+                                    showMessage(
+                                        document.getElementById(
+                                            'passwordNoNumber'
+                                        ),
+                                        e.target.value.search(/[0-9]/) < 0
+                                    );
+                                } else {
+                                    message.style.display = 'none';
+                                }
+                            }}
+                            id="wachtwoord"
+                            required
+                        ></input>
                         <span></span>
-                        <label>Wachtwoord</label>
+                        <label>
+                            Wachtwoord (min. 8 tekens, 1 hoofdletter, 1 kleine
+                            letter, 1 cijfer)
+                        </label>
                     </div>
-                    <input type="submit" value="Inloggen"></input>
+                    <input
+                        type="submit"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            const errorMessages =
+                                document.querySelectorAll('.message > p');
+                            for (let i = 0; i < errorMessages.length; i++) {
+                                const element = errorMessages[i];
+                                if (element.style.display === 'block') {
+                                    alert(
+                                        'De eisen voor uw email of wachtwoord zijn nog niet voldaan'
+                                    );
+                                    return;
+                                }
+                            }
+                            console.log('Eisen zijn voldaan.');
+                        }}
+                        value="Inloggen"
+                    ></input>
                     <div className="pass-adminlogin">
-                        <div className="pass">Wachtwoord vergeten?</div>
-                        <div className="adminlogin_link">
-                            Zoekt u het admin-portal?
-                        </div>
+                        <a href="./forgot_password">Wachtwoord vergeten?</a>
+                        <a
+                            href=""
+                            className="adminlogin_link"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setIsAdminPortal(!isAdminPortal);
+                            }}
+                        >
+                            {isAdminPortal
+                                ? 'Zoekt u naar het klanten-portal?'
+                                : 'Zoekt het admin-portal?'}
+                        </a>
                     </div>
                 </form>
             </div>
