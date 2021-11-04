3a3f247346ab454318b69641a735830612e924cd
Pemseles <TJBouwsema@gmail.com>
2021-11-04 16:06:05 +0100

    some things see description
    + camel case toegepast
    + fotolibrary & verwijder knoppen weg, is nu select knop voor main view
    + react usestate soort van implemented denk ik?
    + user portal toevoegen niet functioneel :) thanks javasript <3 u
    
    foundation voor quinn om van te werken

--- src/pages/admin-portal/admin-portal.js
+++ src/pages/admin-portal/admin-portal.js
@@ -1,20 +1,29 @@
 import * as React from 'react';
 import './admin-portal.css'
-import kyndaletter from './kyndaletter.png';
+import kyndaLetter from './kyndaletter.png';
 import cog from './cog69420.png';
+
+let userPortalList = [];
+DrawUserPortals();
 
 export default {
     url: '/admin-portal',
     Render: (queryParams) => {
+        const [userPortalList1, SetUserPortalList] = React.useState([]);
+        
+        
+        React.useEffect(() => {
+            
+        }) 
         return (
             <React.Fragment>
                 {/* menubar bijna tzelfde als die in user-portal.js */}
-                <div class="menubarAdmin">
-                    <div class="kyndalogo">  
-                        <img src={kyndaletter} width="104" height="55" />
+                <div class="menuBarAdmin">
+                    <div class="kyndaLogo">  
+                        <img src={kyndaLetter} width="104" height="55" />
                     </div>
-                    <div class="adminportalHeader">Adminportaal</div>
-                    <div class="dropdown">
+                    <div class="adminPortalHeader">Adminportaal</div>
+                    <div class="dropDown">
                         <label for="menu"> Opties: </label>
                         <select name="menu" id="menu">
                             <option value="optie1">optie1</option>
@@ -23,22 +32,22 @@ export default {
                             <option value="optie4">optie4</option>
                         </select>
                     </div>
-                    <div class="logoutbutton">
+                    <div class="logOutButton">
                         <loguitbutton>Uitloggen</loguitbutton>
                     </div>
-                    <div class="kyndacog">
+                    <div class="kyndaCog">
                         <img src={cog} width="40" height="40" />
                     </div>
                 </div>
 
-                <div class="mainpage">
-                    <div class="listviewtxtbox"> 
-                        <p class="listviewtxt">User Portals</p>
-                        <p class="downloadsviewtxt">Downloadstatistieken</p>
+                <div class="mainPage">
+                    <div class="listViewTxtBox"> 
+                        <p class="listViewTxt">User Portals</p>
+                        {/*<p class="downloadsviewtxt">Downloadstatistieken</p>*/}
                     </div>
-                    <div class="midsection">
-                        <div class="userportallist">{drawuserportals()}</div>
-                        <div class="downloadstatistics">
+                    <div class="midSection">
+                        <div class="userPortalList" id="userPortalList">{userPortalList}</div>
+                        {/*<div class="downloadstatistics">
                             <div class="templatedownloadsbox">
                                 <div class="statheader">Templates</div>
                                 {drawstatisticstemplates()}
@@ -47,10 +56,10 @@ export default {
                                 <div class="statheader">Designs</div>
                                 {drawstatisticsdesigns()}
                             </div>
-                        </div> 
+                        </div>*/} 
                     </div>
-                    <div class="listviewtxtbox">
-                        <p class="adduserportalbutton">User Portal Toevoegen</p>
+                    <div class="listViewTxtBox">
+                        <p class="addUserPortalButton" onClick={() => SetUserPortalList(AddUserPortal())}>User Portal Toevoegen</p>
                     </div>
                 </div>
             </React.Fragment>
@@ -58,55 +67,51 @@ export default {
     },
 };
 
-function drawuserportals() {
+function DrawUserPortals() {
     // function to generate user-portal list-view
-    window.userportallist = [];
-    window.userportalamount = 10; // temp value, should be amount of user-portals, get from database
-    for (window.listpos = 1; window.listpos <= window.userportalamount; window.listpos++) {
-        window.userportallist.push(<div class="userportalitembox">
-            <div class="userportalitem">
-                {getuserportalid(window.listpos)} <br/>
-                {getuserportalcompany()}
-                <div class="fotolibraryaccessbutton">Open Fotolibrary</div>
-                <div class="deletebutton" onClick={removeuserportal(window.listpos, window.userportallist)}>Delete</div>
+    
+    let userPortalAmount = 10; // temp value, should be amount of user-portals, get from database
+    for (let listPos = 1; listPos <= userPortalAmount; listPos++) {
+        userPortalList.push(<div class="userPortalItemBox">
+            <div class="userPortalItem">
+                {GetUserPortalId(listPos)} <br/>
+                {GetUserPortalCompany()}
+                <div class="selectUserPortalButton">Selecteren</div>
             </div>
         </div>);
     }
-    return window.userportallist;
 }
 
-function getuserportalid(listpos) {
+function GetUserPortalId(listPos) {
     // for filling the individual user-portal boxes with actual info
     // update later to get the user-portal internal id, so company name (& maybe logo) can be retrieved
-    let id = "User Portal " + listpos;
+    let id = "User Portal " + listPos;
     return id;
 }
 
-function getuserportalcompany() {
+function GetUserPortalCompany() {
     // use internal user-portal id to get company name (& maybe logo but idk)
     let companyname = "Temporary Intelligence Incorporated" // temp value, should be the corporation name, get from database
     return companyname
 }
 
-function removeuserportal(listpos, list) {
-    delete list[listpos];
+/*function removeuserportal(listPos, list) {
+    delete list[listPos];
     return list;
-}
+}*/
 
-function adduserportal() {
-    window.userportalamount++;
-    window.userportallist.push(<div class="userportalitembox">
-        <div class="userportalitem">
-            {getuserportalid(window.userportalamount)} <br/>
-            {getuserportalcompany()}
-            <div class="fotolibraryaccessbutton">Open Fotolibrary</div>
-            <div class="deletebutton" onClick={removeuserportal(window.userportalamount, window.userportallist)}>Delete</div>
+function AddUserPortal() {
+    userPortalList.push(<div class="userPortalItemBox">
+        <div class="userPortalItem">
+            {GetUserPortalId(69420 /* change l8er */)} <br/>
+            {GetUserPortalCompany()}
+            <div class="selectUserPortalButton">Selecteren</div>
         </div>
     </div>);
-    return window.userportallist;
+    console.log(userPortalList);
 }
 
-function drawstatisticstemplates() {
+/*function drawstatisticstemplates() {
     let templates = {1: 5, 2: 11, 3: 10, 4: 6, 5: 4, 6: 20, 7: 11, 8: 16, 9: 2, 10: 43}; // temp value, is dictionary with key=templateId & value=downloadamount, get from database (not sure if downloads have to be per template & per user portal or just per template)
     let stats = [];
     for (var key in templates) {
@@ -135,4 +140,4 @@ function drawstatisticsdesigns() {
         );
     }
     return stats;
-}
+}*/
