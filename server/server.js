1b0000bbb3092c1346d94dd1a1478735a22e20cd
Quinn Rachman <quinnrachman@gmail.com>
2021-11-11 04:43:43 +0100

    Little changes

--- server/server.js
+++ server/server.js
@@ -9,16 +9,10 @@ const server = http.createServer();
 function getDataFromRequest(req) {
     return new Promise((resolve, reject) => {
         let stringData = '';
-        let data = {};
 
         req.on('data', (chunk) => (stringData += chunk));
 
         req.on('end', () => {
-            // stringData.split('&').forEach((item) => {
-            //     const splitString = item.split('=');
-            //     data[splitString[0]] = decodeURIComponent(splitString[1]);
-            // });
-            console.log(stringData);
             resolve(JSON.parse(stringData));
         });
     });
@@ -26,13 +20,12 @@ function getDataFromRequest(req) {
 
 // Listen to the request event
 server.on('request', (req, res) => {
-    // We kijken of de request die binnenkomt van localhost:3000 is
     res.setHeader('Access-Control-Allow-Origin', process.env.APP_URL);
+
     if (req.headers.origin === process.env.APP_URL) {
         req.setEncoding('utf-8');
 
         if (req.url === '/auth') {
-            // Hier doen we dan onze standaard dingen
             getDataFromRequest(req).then((data) => {
                 const conn = DBManager.startConnection();
                 conn.runStatement('SELECT * FROM user WHERE Email = ? AND Password = ?', [
@@ -40,11 +33,9 @@ server.on('request', (req, res) => {
                     data.password,
                 ]).then((result) => {
                     if (result.length === 0) {
-                        // res.writeHead(301, { Location: process.env.APP_URL + '/' });
                         res.writeHead(200);
                         res.end(JSON.stringify({ content: { auth: false } }));
                     } else {
-                        // res.writeHead(301, { Location: process.env.APP_URL + '/user-portal' });
                         res.writeHead(200);
                         res.end(
                             JSON.stringify({
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -35,47 +35,33 @@ export default {
                             onClick={(e) => {
                                 e.preventDefault();
 
-                                const sessionEmail = sessionStorage.getItem('email');
-                                const sessionPassword = sessionStorage.getItem('password');
-
-                                if (sessionEmail !== null && sessionPassword !== null) {
-                                    console.log('Not implemented yet');
-                                } else {
-                                    fetch('http://localhost:8080/auth', {
-                                        method: 'POST',
-                                        body: JSON.stringify({
-                                            email: document.getElementById('email').value,
-                                            password: document.getElementById('password').value,
-                                        }),
-                                    })
-                                        .then((res) => res.json())
-                                        .then((data) => {
-                                            if ('auth' in data.content) {
-                                                if (data.content.auth) {
-                                                    sessionStorage.setItem(
-                                                        'email',
-                                                        document.getElementById('email').value
-                                                    );
-                                                    sessionStorage.setItem(
-                                                        'password',
-                                                        document.getElementById('password').value
-                                                    );
-
-                                                    data.content.isAdmin
-                                                        ? (window.location.href =
-                                                              window.location.href + 'admin-portal')
-                                                        : (window.location.href =
-                                                              window.location.href + 'user-portal');
-                                                } else {
-                                                    setIsAuthError(true);
-                                                }
+                                // Hier maken wij gebruik van de fetch method om een post request
+                                // te sturen naar de url die wordt meegegeven.
+                                // Voor meer informatie over fetch kan je naar deze url gaan
+                                // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+                                fetch('http://localhost:8080/auth', {
+                                    method: 'POST',
+                                    body: JSON.stringify({
+                                        email: document.getElementById('email').value,
+                                        password: document.getElementById('password').value,
+                                    }),
+                                })
+                                    .then((res) => res.json())
+                                    .then((data) => {
+                                        if ('auth' in data.content) {
+                                            if (data.content.auth) {
+                                                data.content.isAdmin
+                                                    ? (window.location.href =
+                                                          window.location.href + 'admin-portal')
+                                                    : (window.location.href =
+                                                          window.location.href + 'user-portal');
                                             } else {
-                                                console.error(
-                                                    'No auth. Request not made to auth url?'
-                                                );
+                                                setIsAuthError(true);
                                             }
-                                        });
-                                }
+                                        } else {
+                                            console.error('No auth. Request not made to auth url?');
+                                        }
+                                    });
                             }}
                         ></input>
                         <div className="pass-adminlogin">
@@ -87,34 +73,3 @@ export default {
         );
     },
 };
-
-function emailValidation(e) {
-    const message = document.getElementById('message');
-    const showMessage = (el, cond) =>
-        cond ? (el.style.display = 'block') : (el.style.display = 'none');
-    if (e.target.value.length > 0) {
-        message.style.display = 'block';
-        showMessage(document.getElementById('emailApenstaartje'), e.target.value.indexOf('@') < 0);
-        showMessage(document.getElementById('emailSpatie'), e.target.value.indexOf(' ') >= 0);
-    } else {
-        message.style.display = 'none';
-    }
-}
-
-function passwordValidation(e) {
-    const message = document.getElementById('message');
-    const showMessage = (el, cond) =>
-        cond ? (el.style.display = 'block') : (el.style.display = 'none');
-    if (e.target.value.length > 0) {
-        message.style.display = 'block';
-        showMessage(document.getElementById('passwordShort'), e.target.value.length < 8);
-        showMessage(document.getElementById('passwordNoLower'), e.target.value.search(/[a-z]/) < 0);
-        showMessage(document.getElementById('passwordNoUpper'), e.target.value.search(/[A-Z]/) < 0);
-        showMessage(
-            document.getElementById('passwordNoNumber'),
-            e.target.value.search(/[0-9]/) < 0
-        );
-    } else {
-        message.style.display = 'none';
-    }
-}
