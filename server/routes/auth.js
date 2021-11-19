8b118eec810c0471f221748d05c5fd4990caf06d
Quinn Rachman <quinnrachman@gmail.com>
2021-11-19 20:06:35 +0100

    Changed the way auth.js is written with the new helpers.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,5 @@
 const RequestHelper = require("../src/RequestHelper");
+const ResponseHelper = require("../src/ResponseHelper");
 const DBManager = new (require("../src/db/DB"))();
 const Token = new (require('../src/Token'))();
 
@@ -6,60 +7,48 @@ module.exports = {
     name: 'auth',
     type: 'GET',
     action: (req, res) => {
+        const responseHelper = new ResponseHelper(res);
+        const requestHelper = new RequestHelper(req);
+
         if ('authorization' in req.headers) {
-            const token = req.headers.authorization.split(' ')[1];
+            const token = requestHelper.getRequestToken();
             
             if (Token.verifyJWT(token)) {
-                res.writeHead(200);
-                res.end(
-                    JSON.stringify({
-                        content: { token: token },
-                    })
-                );
+                responseHelper.responseSuccess({ token: token });
             } else {
-                res.writeHead(200);
-                res.end(JSON.stringify({ content: { token: null } }));
+                responseHelper.responseSuccess({ token: null });
             }
         } else {
-            const requestHelper = new RequestHelper(req);
-    
-            requestHelper.getDataFromRequest()
-            .then(data => {
-                const conn = DBManager.startConnection();
-                conn.runStatement('SELECT * FROM user WHERE Email = ? AND Password = ?', [
-                    data.email,
-                    data.password,
-                ]).then((result) => {
+            (async () => {
+                try {
+                    const conn = DBManager.startConnection();
+                    const data = await requestHelper.getRequestData();
+                    const result = await conn.runStatement('SELECT * FROM user WHERE Email = ? AND Password = ?', [
+                        data.email,
+                        data.password,
+                    ]);
                     if (result.length === 0) {
                         res.writeHead(200);
-                        res.end(JSON.stringify({ content: { token: null } }));
+                        res.end(responseHelper.responseSuccess({ token: token }));
                     } else {
                         const token = Token.createJWT({
                             sub: result[0].Id,
                             email: result[0].Email,
                             admin: result[0].Role_Id === 1
                         });
-                        
-                        res.writeHead(200);
-                        res.end(
-                            JSON.stringify({
-                                content: { token: token },
-                            })
-                        );
+
+                        responseHelper.responseSuccess({ token: token })
                     }
-                });
-                conn.endConnection();
-            })
-            .catch(err => {
-                if (err.name === "SyntaxError") {
-                    // No valid json send through request.
-                    // Send response with error.
-                    res.writeHead(200);
-                    res.end(JSON.stringify({
-                        content: { err: "No valid json body send." },
-                    }))
+
+                    conn.endConnection();
+                } catch (error) {
+                    if (error.name === "SyntaxError") {
+                        // No valid json send through request.
+                        // Send response with error.
+                        responseHelper.responseInvalidJson();
+                    }
                 }
-            });
+            })();
         }
     }
 }
