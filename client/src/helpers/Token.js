3e28bad318b048a9a1a5943aa17796c3652dd16b
Quinn Rachman <quinnrachman@gmail.com>
2021-11-24 00:01:22 +0100

    Token helper added for retrieving a token and checking if a token exists

---
+++ client/src/helpers/Token.js
@@ -0,0 +1,12 @@
+export const getToken = () => {
+    try {
+        return document.cookie.split(';').find(row => row.startsWith('token=')).split('=')[1];
+    } catch (error) {
+        return false;
+    }
+}
+
+
+export const tokenExists = () => {
+    return getToken() instanceof String;
+}
