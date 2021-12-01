96daa41cfa8bd72eec7ebe003611a78e90049aab
Quinn Rachman <quinnrachman@gmail.com>
2021-12-01 16:23:27 +0100

    Twee delete routes aangepast. Eerste is de delete image route die nu ook daadwerkelijk de afbeelding verwijderen en dat niet alleen de record uit de sql database wordt verwijderd. De tweede is de delete company route die ervoor zorgt dat bij het verwijderen niet alleen de record uit de database wordt gehaald maar ook de directories die bij de company horen in de storage folder. Verder nog addCompany method een sync optie gegeven en gitignore toegevoegd in storage folder zodat die mee wordt gecommit

--- server/routes.js
+++ server/routes.js
@@ -1,50 +1,179 @@
-const TableStructure = require('./TableStructure');
-const Route = new (require('./src/Route'))();
+const TableStructure = require("./TableStructure");
+const Route = new (require("./src/Route"))();
 const DBManager = new (require("./src/db/DB"))();
-const Token = new (require('./src/Token'))();
+const Token = new (require("./src/Token"))();
+const Storage = new (require("./src/Storage"))();
 
-Route.add('/auth', async (req, res) => {
-    try {
-        if ('authorization' in req.request.headers) {
-            const token = req.getRequestToken();
+Route.add("/auth", async (req, res) => {
+  try {
+    if ("authorization" in req.request.headers) {
+      const token = req.getRequestToken();
 
-            if (Token.verifyJWT(token)) {
-                res.responseSuccess({ token: token });
-            } else {
-                res.responseSuccess({ token: null });
-            }
-        } else {
-            const conn = DBManager.startConnection();
+      if (Token.verifyJWT(token)) {
+        res.responseSuccess({ token: token });
+      } else {
+        res.responseSuccess({ token: null });
+      }
+    } else {
+      const conn = DBManager.startConnection();
 
-            const data = await req.getRequestData();
+      const data = await req.getRequestData();
 
-            const result = await conn.runStatement('SELECT * FROM user WHERE Email = ? AND Password = ?', [
-                data.email,
-                data.password,
-            ]);
+      const result = await conn.runStatement(
+        "SELECT * FROM user WHERE Email = ? AND Password = ?",
+        [data.email, data.password]
+      );
 
-            if (result.length === 0) {
-                res.responseSuccess({ token: null });
-            } else {
-                const role = await conn.runStatement('SELECT Name FROM role WHERE Id = ?', [result[0].Role_Id]);
+      if (result.length === 0) {
+        res.responseSuccess({ token: null });
+      } else {
+        const role = await conn.runStatement(
+          "SELECT Name FROM role WHERE Id = ?",
+          [result[0].Role_Id]
+        );
 
-                const token = Token.createJWT({
-                    sub: result[0].Id,
-                    email: result[0].Email,
-                    company: result[0].Company_Id,
-                    type: role[0].Name
-                });
+        const token = Token.createJWT({
+          sub: result[0].Id,
+          email: result[0].Email,
+          company: result[0].Company_Id,
+          type: role[0].Name,
+        });
 
-                res.responseSuccess({ token: token })
-            }
+        res.responseSuccess({ token: token });
+      }
 
-            conn.endConnection();
-        }
-    } catch (error) {
-        return error;
+      conn.endConnection();
     }
+  } catch (error) {
+    return error;
+  }
 });
 
-Route.addCrudFromTableStructure(TableStructure);
+// Adds routes based on the existing table structure defined in TableStructure.js
+
+for (let i = 0; i < TableStructure.length; i++) {
+  const table = TableStructure[i];
+
+  if (table.name === "image") {
+    Route.add(`/${table.name}/create`, async (req, res) => {
+      try {
+        const requestBody = await req.getRequestData();
+        const payload = req.getPayload();
+
+        const conn = DBManager.startConnection();
+
+        // create special exception to notify if body is missing or empty
+        const date = new Date().toLocaleDateString();
+
+        Storage.addImage(requestBody.name, payload.company, requestBody.image);
+
+        await conn.runStatement(
+          `
+                            INSERT INTO ${table.name} (${table.columns.join()})
+                            VALUES (${table.columns.map((val) => "?").join()})`,
+          [pathToFile, date, date, payload.company]
+        );
+
+        res.responseSuccess();
+      } catch (error) {
+        return error;
+      }
+    });
+
+    Route.add(`/${table.name}/delete`, async (req, res) => {
+      try {
+        const conn = DBManager.startConnection();
+
+        const requestBody = await req.getRequestData();
+
+        // Directory unlink
+        const file = await conn.runStatement(
+          `
+                    SELECT Filepath FROM ${table.name} WHERE Id = ?`,
+          [requestBody.id]
+        );
+
+        const filePath = file[0].Filepath;
+
+        Storage.removeImage(filePath);
+
+        const result = await conn.runStatement(
+          `DELETE FROM ${table.name} WHERE Id = ?`,
+          [requestBody.id]
+        );
+
+        res.responseSuccess(result);
+      } catch (error) {
+        return error;
+      }
+    });
+
+    Route.addAll(table);
+    Route.addRead(table);
+    Route.addUpdate(table);
+
+    continue;
+  }
+
+  if (table.name === "company") {
+    Route.add(`/${table.name}/create`, async (req, res) => {
+      try {
+        const requestBody = await req.getRequestData();
+
+        const conn = DBManager.startConnection();
+
+        // create special exception to notify if body is missing or empty
+        await conn.runStatement(
+          `
+                            INSERT INTO ${table.name} (${table.columns.join()})
+                            VALUES (${requestBody.values
+                              .map((val) => "?")
+                              .join()})`,
+          requestBody.values
+        );
+
+        const companyId = await conn.runStatement(
+          `
+                            SELECT Id FROM ${table.name} WHERE Email = ?
+                        `,
+          [requestBody.values[2]]
+        );
+
+        Storage.addCompany(companyId[0].Id);
+
+        res.responseSuccess();
+      } catch (error) {
+        return error;
+      }
+    });
+
+    Route.add(`/${table.name}/delete`, async (req, res) => {
+      try {
+        const conn = DBManager.startConnection();
+
+        const requestBody = await req.getRequestData();
+
+        Storage.removeCompany(requestBody.id);
+
+        const result = await conn.runStatement(
+          `DELETE FROM ${table.name} WHERE Id = ?`,
+          [requestBody.id]
+        );
+
+        res.responseSuccess(result);
+      } catch (error) {
+        return error;
+      }
+    });
+
+    Route.addAll(table);
+    Route.addRead(table);
+    Route.addUpdate(table);
+
+    continue;
+  }
+
+  Route.addCrud(table);
+}
 
 module.exports = Route.urlCollection;
--- server/src/Storage.js
+++ server/src/Storage.js
@@ -1,24 +1,89 @@
-const pathLib = require('path');
-const fs = require('fs');
+const pathLib = require("path");
+const fs = require("fs/promises");
 
 class Storage {
-    constructor(path = null) {
-        this.storagePath = path === null ? pathLib.normalize(process.cwd() + "/storage") : path;
+  constructor(path = null) {
+    this.storagePath =
+      path === null ? pathLib.normalize(process.cwd() + "/storage") : path;
+  }
+
+  async addCompany(companyName, sync = false) {
+    if (!sync) {
+      fs.mkdir(
+        pathLib.normalize(this.storagePath + `/${companyName}/images`),
+        { recursive: true },
+        (err) => {
+          if (err) throw err;
+        }
+      );
+
+      fs.mkdir(
+        pathLib.normalize(this.storagePath + `/${companyName}/templates`),
+        { recursive: true },
+        (err) => {
+          if (err) throw err;
+        }
+      );
+
+      fs.mkdir(
+        pathLib.normalize(this.storagePath + `/${companyName}/designs`),
+        { recursive: true },
+        (err) => {
+          if (err) throw err;
+        }
+      );
+    } else {
+      await fs.mkdir(
+        pathLib.normalize(this.storagePath + `/${companyName}/images`),
+        { recursive: true },
+        (err) => {
+          if (err) throw err;
+        }
+      );
+
+      await fs.mkdir(
+        pathLib.normalize(this.storagePath + `/${companyName}/templates`),
+        { recursive: true },
+        (err) => {
+          if (err) throw err;
+        }
+      );
+
+      await fs.mkdir(
+        pathLib.normalize(this.storagePath + `/${companyName}/designs`),
+        { recursive: true },
+        (err) => {
+          if (err) throw err;
+        }
+      );
     }
+  }
 
-    addCompany(companyName) {
-        fs.mkdir(pathLib.normalize(this.storagePath + `/${companyName}/images`), {recursive: true}, (err) => {
-            if (err) throw err;
-        })
+  removeCompany(companyId) {
+    fs.rm(
+      pathLib.normalize(this.storagePath + `/${companyId}`),
+      { recursive: true, force: true },
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
 
-        fs.mkdir(pathLib.normalize(this.storagePath + `/${companyName}/templates`), { recursive: true }, (err) => {
-            if (err) throw err;
-        })
+  addImage(fileName, companyId, dataUrl) {
+    fs.appendFile(
+      pathLib.normalize(this.storagePath + `/${companyId}/images/${fileName}`),
+      Buffer.from(dataUrl.split(",")[1], "base64"),
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
 
-        fs.mkdir(pathLib.normalize(this.storagePath + `/${companyName}/designs`), { recursive: true }, (err) => {
-            if (err) throw err;
-        })
-    }
+  removeImage(filePath) {
+    fs.rm(filePath, {}, (err) => {
+      if (err) throw err;
+    });
+  }
 }
 
 module.exports = Storage;
