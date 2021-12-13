e8a1f44972c2b0859439c90f68f057e91b89fb77
Quinn Rachman <quinnrachman@gmail.com>
2021-12-13 18:15:58 +0100

    Prepare project for deployement

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const RequestHelper = require("./src/RequestHelper");
 const ResponseHelper = require("./src/ResponseHelper");
 const Token = new (require("./src/Token"))();
 const fs = require("fs");
+const path = require("path");
 
 // Create a local server to receive data from
 const server = http.createServer();
@@ -22,8 +23,29 @@ server.on("request", (req, res) => {
     "DELETE",
   ]);
 
+  req.setEncoding("utf-8");
+
   const reqHelper = new RequestHelper(req);
   const resHelper = new ResponseHelper(res);
+
+  if (process.env.APP_ENV === "production") {
+    if (req.url === "/") {
+      fs.readFile(
+        path.normalize(__dirname + "/index.html"),
+        function (err, data) {
+          if (err) {
+            resHelper.responseError(err);
+            return;
+          }
+
+          res.writeHead(200);
+          res.end(data);
+        }
+      );
+
+      return;
+    }
+  }
 
   if (req.url.startsWith("/storage")) {
     fs.readFile(__dirname + req.url, function (err, data) {
@@ -38,52 +60,49 @@ server.on("request", (req, res) => {
 
     return;
   }
-  if (req.headers.origin === process.env.APP_URL) {
-    req.setEncoding("utf-8");
 
-    for (let i = 0; i < routes.length; i++) {
-      const route = routes[i];
+  for (let i = 0; i < routes.length; i++) {
+    const route = routes[i];
 
-      if (req.url === route.url) {
-        if (
-          req.url !== "/auth" &&
-          (!reqHelper.authorizationHeaderExists() ||
-            !Token.verifyJWT(reqHelper.getRequestToken()))
-        ) {
-          resHelper.responseInvalidToken();
-          break;
-        }
-
-        (async () => {
-          try {
-            const result = await route.action(reqHelper, resHelper);
-
-            if (result instanceof Error) throw result;
-          } catch (error) {
-            if (error instanceof SyntaxError) {
-              console.error(error);
-              resHelper.responseInvalidJson();
-            } else if (
-              error instanceof Error &&
-              "code" in error &&
-              error.code === "SQLITE_ERROR"
-            ) {
-              console.error(error);
-              resHelper.responseInvalidCrudData();
-            } else {
-              console.error(error);
-              resHelper.responseError(error.message);
-            }
-          }
-        })();
-
+    if (req.url === route.url) {
+      if (
+        req.url !== "/auth" &&
+        (!reqHelper.authorizationHeaderExists() ||
+          !Token.verifyJWT(reqHelper.getRequestToken()))
+      ) {
+        resHelper.responseInvalidToken();
         break;
       }
+
+      (async () => {
+        try {
+          const result = await route.action(reqHelper, resHelper);
+
+          if (result instanceof Error) throw result;
+        } catch (error) {
+          if (error instanceof SyntaxError) {
+            console.error(error);
+            resHelper.responseInvalidJson();
+          } else if (
+            error instanceof Error &&
+            "code" in error &&
+            error.code === "SQLITE_ERROR"
+          ) {
+            console.error(error);
+            resHelper.responseInvalidCrudData();
+          } else {
+            console.error(error);
+            resHelper.responseError(error.message);
+          }
+        }
+      })();
+
+      break;
     }
-  } else {
-    res.writeHead(404);
-    res.end();
   }
+
+  res.writeHead(404);
+  res.end();
 });
 
-server.listen(8080);
+server.listen(3001);
