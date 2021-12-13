da0027d55b21c9544b2144475b9f2cbd57c32a10
Rémon Zander (0948619) <0948619@hr.nl>
2021-12-13 14:43:31 +0100

    added menu bar sort off. We need help Guido

--- client/src/pages/user-portal/user-portal.js
+++ client/src/pages/user-portal/user-portal.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './user-portal.css';
-import kyndaletter from './kyndaletter.png';
+import kyndalogo from './kyndaletter.png';
 import cog from './cog69420.png';
 import plus from './plusafbeelding.png';
 import { CreateExport } from '../../helpers/Export';
@@ -20,8 +20,62 @@ import {
 import { Settings } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+    icon: {
+        marginRight: '20px',
+    },
+    cardGrid: {
+        padding: '20px 0',
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%',
+        width: '100%',
+        height: '100%',
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+}));
+
 function UserPortal() {
-    return <React.Fragment></React.Fragment>;
+    const styles = useStyles();
+    return (
+        <React.Fragment>
+            <AppBar position="relative" style={{ background: 'white' }}>
+                <Toolbar>
+                    <img
+                        src={kyndalogo}
+                        alt="kynda logo"
+                        width="90"
+                        height="30"
+                        style={{ marginRight: '20px' }}
+                    />
+                    <Typography variant="h5" style={{ color: 'black' }}>
+                        User portal
+                    </Typography>
+                    <Grid container spacing={2} justifyContent="flex-end">
+                        <Grid item>
+                            <Button variant="contained" color="primary">
+                                Uitloggen
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Settings
+                                className={styles.icon}
+                                style={{ color: 'black' }}
+                                fontSize="large"
+                            />
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+        </React.Fragment>
+    );
 }
 
 export default CreateExport('/user-portal', UserPortal);
