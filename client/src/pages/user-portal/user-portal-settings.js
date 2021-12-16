e82d88166f52ddafe417d2fb8bfc7f8b9913868f
Rémon Zander (0948619) <0948619@hr.nl>
2021-12-16 22:41:18 +0100

    almost completed the settingspage

--- client/src/pages/user-portal/user-portal-settings.js
+++ client/src/pages/user-portal/user-portal-settings.js
@@ -20,6 +20,7 @@ import {
     ListItem,
     Divider,
     TextField,
+    Container
 } from '@material-ui/core';
 import { AccountCircle, BorderBottom, Security, SquareFoot } from '@material-ui/icons';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
@@ -69,7 +70,7 @@ function UserPortalSettings() {
             let usersOfCompany = [];
             for (let userIndex = 0; userIndex < allUsers.length; userIndex++) {
                 if (
-                    //allUsers[userIndex].Company_Id === user.company &&
+                    allUsers[userIndex].Company_Id === user.company &&
                     allUsers[userIndex].Email !== user.email
                 ) {
                     usersOfCompany.push(allUsers[userIndex]);
@@ -186,33 +187,110 @@ function UserPortalSettings() {
                     <Divider />
                 </List>
                 <List>
+                    {user.type === 'Moderator' ? <ListItem style={{ alignItems: 'center', justifyContent: 'center', marginBottom: '10px' }}>
+                            <Typography variant="h6">
+                            Hieronder kunt u een gebruiker selecteren om hoofdgebruiker te maken.<br />
+                            Er kan maar een hoofdgebruiker zijn. Dit betekent dat <br/>het huidige hoofdgebruiker account een normale gebruiker wordt.
+                            </Typography>
+                        </ListItem> : ''}
                     {user.type === 'Moderator'
-                    ? userList.map((user, index) => {
-                        if (index % 2 === 0) {
+                        ? userList.map((user, index) => {
+                            if (index % 2 === 0) {
+                                console.log(index + ' ' + userList.length);
                             return (
-                                <><ListItem style={{ alignItems: 'center', justifyContent: 'center' }}>
-                                    <ListItem style={{ alignItems: 'center' }}>
+                                <><ListItem style={{ alignItems: 'center', justifyContent: 'center', marginRight: '50px'}}>
+                                    {index === userList.length - 1 && userList.length % 2 !== 0 ? <><ListItem id={'userField' + index} style={{ alignItems: 'center', width: '50%' }}
+                                        onMouseEnter={() => userOnHover(index)}
+                                        onMouseLeave={() => userLeave(index)}>
                                         <AccountCircle style={{ fontSize: '60px', marginRight: '15px' }} />
                                         <Typography variant="h6">
                                             {userList[index].Name}
                                             <br />
                                             {userList[index].Email}
                                         </Typography>
-                                    </ListItem>
-                                    <ListItem style={{ alignItems: 'center', marginLeft: '100px' }}>
+                                    </ListItem><Button
+                                        variant="contained"
+                                        color="primary"
+                                        style={{
+                                            position: 'absolute',
+                                            top: '30px',
+                                            left: '275px',
+                                            opacity: 0
+                                        }}
+                                        id={'userButton' + index}
+                                        onMouseEnter={() => userOnHover(index)}
+                                        onMouseLeave={() => userLeave(index)}
+                                        onClick={() => onMakeMainUserButtonClick(user, getPayloadAsJson().sub)}
+                                    >
+                                            Maak hoofdgebruiker
+                                        </Button></> : <><ListItem id={'userField' + index} style={{ alignItems: 'center', width: '50%' }}
+                                            onMouseEnter={() => userOnHover(index)}
+                                            onMouseLeave={() => userLeave(index)}>
+                                            <AccountCircle style={{ fontSize: '60px', marginRight: '15px' }} />
+                                            <Typography variant="h6">
+                                                {userList[index].Name}
+                                                <br />
+                                                {userList[index].Email}
+                                            </Typography>
+                                        </ListItem><Button
+                                            variant="contained"
+                                            color="primary"
+                                            style={{
+                                                position: 'absolute',
+                                                top: '30px',
+                                                left: '100px',
+                                                opacity: 0
+                                            }}
+                                            id={'userButton' + index}
+                                            onMouseEnter={() => userOnHover(index)}
+                                            onMouseLeave={() => userLeave(index)}
+                                            onClick={() => onMakeMainUserButtonClick(user, getPayloadAsJson().sub)}
+                                        >
+                                                Maak hoofdgebruiker
+                                            </Button></>}
+                                    {index + 1 < userList.length ? <><ListItem id={'userField' + (index + 1)} style={{ alignItems: 'center', marginLeft: '50px', width: '50%' }}
+                                        onMouseEnter={() => userOnHover(index + 1)}
+                                        onMouseLeave={() => userLeave(index + 1)}>
                                         <AccountCircle style={{ fontSize: '60px', marginRight: '15px' }} />
                                         <Typography variant="h6">
                                             {userList[index + 1].Name}
                                             <br />
                                             {userList[index + 1].Email}
                                         </Typography>
-                                    </ListItem>
+                                    </ListItem><Button
+                                        variant="contained"
+                                        color="primary"
+                                        style={{
+                                            position: 'absolute',
+                                            top: '30px',
+                                            left: '477px',
+                                            opacity: 0
+                                        }}
+                                        id={'userButton' + (index + 1)}
+                                        onMouseEnter={() => userOnHover(index + 1)}
+                                        onMouseLeave={() => userLeave(index + 1)}
+                                        onClick={() => onMakeMainUserButtonClick(userList[index + 1], getPayloadAsJson().sub)}
+                                    >
+                                            Maak hoofdgebruiker
+                                        </Button></> : ''}
                                     
                                     </ListItem><Divider /></>
                             );
                         }
-                            })
+                        })
                         : ''}
+                </List>
+                <Typography variant="h6">
+                    Gebruiker toevoegen:
+                </Typography>
+                <List>
+                    <ListItem>
+                        <AccountCircle style={{ fontSize: '100px', marginRight: '15px' }} />
+                        <div style={{fontSize: '20px'}}>Naam: <TextField required /><br/>
+                            Email: <TextField required /><br/>
+                            Wachtwoord: <TextField required />
+                        </div>
+                    </ListItem>
                 </List>
             </Box>
         </React.Fragment>
@@ -221,6 +299,67 @@ function UserPortalSettings() {
 
 export default CreateExport('/user-portal-settings', UserPortalSettings);
 
+async function onMakeMainUserButtonClick(user, currentUserId) {
+    const ApiInstance = new Api(getToken());
+    let userDataDb = [];
+    if (typeof (userDataDb = await ApiInstance.read('user', currentUserId)) === 'undefined' || userDataDb.status === 'FAIL') {
+        window.alert('De verbinding met de database is verbroken. Probeer het later opnieuw.');
+        return;
+    }
+
+    let result = [];
+    if (typeof (result = await ApiInstance.update('user', currentUserId,
+        [
+            userDataDb.content[0].Email,
+            userDataDb.content[0].Password,
+            3,
+            userDataDb.content[0].Name,
+            userDataDb.content[0].Company_Id
+        ])) === 'undefined') {
+        window.alert(
+            'De verbinding met de database is verbroken. Probeer het later opnieuw.'
+        );
+        return;
+    }
+
+    if (typeof (result = await ApiInstance.update('user', user.Id,
+        [
+            user.Email,
+            user.Password,
+            2,
+            user.Name,
+            user.Company_Id
+        ])) === 'undefined') {
+        window.alert(
+            'De verbinding met de database is verbroken. Probeer het later opnieuw.'
+        );
+        return;
+    }
+
+    document.cookie = document.cookie.substring(document.cookie.indexOf('token='), 6);
+    window.location.replace('/').focus();
+}
+
+function userOnHover(id) {
+    const userField = 'userField' + id;
+    const userButtonId = 'userButton' + id;
+    document.getElementById(userField).style.filter = 'blur(4px)';
+    document.getElementById(userField).style.transition = '1s';
+    document.getElementById(userButtonId).style.transition = '1s';
+    document.getElementById(userButtonId).style.opacity = '1';
+    document.getElementById(userButtonId).style.top =
+        String(document.getElementById(userField).height / 1.5) + 'px';
+    document.getElementById(userButtonId).style.left =
+        String(document.getElementById(userField).width / 7) + 'px';
+}
+
+function userLeave(id) {
+    const userField = 'userField' + id;
+    const userButtonId = 'userButton' + id;
+    document.getElementById(userField).style.filter = 'none';
+    document.getElementById(userButtonId).style.opacity = '0';
+}
+
 async function GetUserPassword(userInstance) {
     let userDataDb = [];
     const ApiInstance = new Api(getToken());
@@ -228,17 +367,10 @@ async function GetUserPassword(userInstance) {
         window.alert('De verbinding met de database is verbroken. Probeer het later opnieuw.');
         return;
     }
-    // console.log(userDataDb);
-    // console.log(userDataDb.content);
-    console.log(userInstance);
     return userDataDb.content[0].Password;
 }
 
 async function ChangePass(userId, userPassword, currentPass, newPass, confirmPass, setPassError) {
-    console.log(currentPass);
-    console.log(newPass);
-    console.log(confirmPass);
-
     setPassError([
         currentPass === '' ? 'Dit veld is verplicht' : currentPass !== userPassword ? 'Het wachtwoord is onjuist' : '', 
         newPass === '' ? 'Dit veld is verplicht' : '', 
@@ -249,7 +381,6 @@ async function ChangePass(userId, userPassword, currentPass, newPass, confirmPas
     // check for password format; minimaal 8 tekens, 1+ hoofdletter, 1+ cijfer & 1+ speciaal teken
     if (newPass !== confirmPass || currentPass !== userPassword) return;
 
-    console.log(newPass.length);
     if (['!', '@', '#', '$', '%', '^', '&', ' *', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', `|`, ';', ':', "'", '"', ',', '<', '.', '>', '/', '?', '`', '~'].some(s => newPass.includes(s)) &&
         ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].some(s => newPass.includes(s)) && newPass.length > 7) {
         console.log('succes');
